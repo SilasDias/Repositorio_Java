67a4a67779ff943d4f6a13480d85eeb494e6428d
SilasDias <silasdjd@gmail.com>
2019-05-18 13:49:11 -0300

    Concluido

---
+++ Cap03_Ex01.java
@@ -0,0 +1,25 @@
+/* Elaborar um programa que leia dois valores numéricos inteiros, some-os e apresente o resultado, caso o valor somado seja maior que 10. */
+import java.io.*; 
+import java.util.Scanner;
+
+class Cap03_Ex01 { 
+public static void main(String args[]) {
+
+int A, B, R; 
+
+Scanner s = new Scanner(System.in);
+
+System.out.println(); 
+System.out.print("Entre o valor <A>: ");
+A = s.nextInt(); 
+System.out.print("Entre o valor <B>: ");
+B = s.nextInt();
+R = A + B; 
+System.out.println();
+ if (R > 10) {
+System.out.print("Resultado = " + R);
+System.out.println();
+} 
+}
+}
+
