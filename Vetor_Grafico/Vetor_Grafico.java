72ee20d4380b3a6c88ea3dae8a8310becf59738a
SilasDias <silasdjd@gmail.com>
2019-05-27 11:47:44 -0300

    Concluido

---
+++ Vetor_Grafico/Vetor_Grafico.java
@@ -0,0 +1,25 @@
+// programa de impressão de gráfico de barras.
+public class Vetor_Grafico
+{
+public static void main(String[] args)
+{
+int[] array = { 12, 11, 10, 9, 8, 7, 6, 5, 4, 2, 1 };
+System.out.println("Grade de Distribuição:");
+// para cada elemento de array, gera saída de uma barra do gráfico
+for (int counter = 0; counter < array.length; counter++)
+{
+// gera saída do rótulo de barra ( "00-09: ", ..., "90-99: ", "100: ")
+if (counter == 10)
+System.out.printf("%5d: ", 100);
+else
+System.out.printf("%02d-%02d: ",
+counter * 10, counter * 10 + 9);
+
+// imprime a barra de asteriscos
+for (int stars = 0; stars < array[counter]; stars++)
+System.out.print("*");
+
+System.out.println();
+}
+}
+} // fim da classe
