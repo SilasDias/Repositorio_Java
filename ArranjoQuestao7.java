9dc1bfe6c6825e80042712486d2764aa87c027b8
SilasDias <silasdjd@gmail.com>
2019-06-01 20:56:05 -0300

    Concluido

---
+++ ArranjoQuestao7.java
@@ -0,0 +1,10 @@
+class ArranjoQuestao7 {
+public static void main(String[] args) {
+String[] str = {"abc", "de", "fghi"};
+System.out.println(str.length);
+for(String s : str)
+System.out.println(s);
+System.out.println(str[1].length());
+}
+}
+
