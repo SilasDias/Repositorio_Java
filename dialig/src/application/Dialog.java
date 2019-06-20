7f589851b8a398e01bbfc9d7c35337df3ad7fad1
SilasDias <silasdjd@gmail.com>
2019-06-20 14:15:25 -0300

    Concluido

---
+++ dialig/src/application/Dialog.java
@@ -0,0 +1,14 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+public class Dialog {
+
+	public static void main(String[] args) {
+		
+		// exibe um diálogo com uma mensagem
+		JOptionPane.showMessageDialog(null,"Welcome to Java");
+		
+	}
+
+}
