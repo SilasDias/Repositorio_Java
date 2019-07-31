95a28b09c67b487d89d1b00e1080bede39acdbdc
SilasDias <silasdjd@gmail.com>
2019-07-30 21:39:30 -0300

    Concluido

---
+++ Curso.java
@@ -0,0 +1,15 @@
+import java.util.Locale;	
+import java.io.*; 
+
+class Curso{
+	public static void main(String[] args) {
+		
+		char gender = 'F';
+		int age = 32;
+		double balance = 10.35784;
+		String name = "Maria";
+		
+		System.out.printf("%s is %d years old, gender %c, and got balance = %.2f bitcoin. %n",name,age, gender, balance);
+	}
+}
+
