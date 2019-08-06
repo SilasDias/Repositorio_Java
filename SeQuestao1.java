ccf6626150ed0dd3fd1725b4b11db1f82286f1ab
SilasDias <silasdjd@gmail.com>
2019-08-05 21:02:19 -0300

    Concluido

---
+++ SeQuestao1.java
@@ -0,0 +1,16 @@
+class SeQuestao1 {
+public static void main(String[] args) {
+int num = 567;
+if(num % 2 == 0) {
+System.out.print("a");
+} else if(num % 3 == 0) {
+System.out.print("b");
+} else if(num % 5 == 0) {
+System.out.print("c");
+} else if(num % 7 == 0) {
+System.out.print("d");
+} else {
+System.out.print("e");
+}
+}
+}
